54b5dcb887fd1c930e5eac22399172746468388c
jeronimonegretea <jeronimonegretea@ielacandelariamedellin.edu.co>
2026-03-24 21:17:39 +0000

    ejercicio}

---
+++ 24_03/ejercicio1-1.js
@@ -0,0 +1,7 @@
+var nombre = "jeronimo"
+let edad = 16
+const ciudad ="medellin"
+console.log(nombre);
+console.log("edad:",edad);
+console.log("ciudad:",ciudad);
+console.log("nombre:",nombre,"edad:",edad,"ciudad:",ciudad);
---
+++ 24_03/ejercicio1-3.js
@@ -0,0 +1,9 @@
+let kilometrage = 20000;
+let kilometrage = 25000;
+let kilometrage = 27000;
+
+const altura = 3;
+altura = 4;
+console.log(altura);
+
+//no se pueden reasignar las variables declaradas con const, por lo tanto, el codigoanterior generara un error al intentar asignar un error al intenetar asignarun nuevo valor a la variable "altura"//

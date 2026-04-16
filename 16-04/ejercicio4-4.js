3f06dda2d4856c0fe4b7e23a2f71dfca68c1482b
jeronimonegretea <jeronimonegretea@ielacandelariamedellin.edu.co>
2026-04-16 21:06:15 +0000

    ejercicios

---
+++ 16-04/ejercicio4-4.js
@@ -0,0 +1,20 @@
+let edad =16;
+let PienePermiso = true;
+if(edad >= 18){
+if(TienePermiso){
+    console.log("Puede entrar");
+}else{
+    console.log("Necesita Permiso");
+}{
+    console.log("No Puede Entrar");
+}
+ 
+}
+
+
+
+
+
+
+
+
